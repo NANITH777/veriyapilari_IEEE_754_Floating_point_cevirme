43c14ab768e2afde0775e7a44e433d2f35c1ab73
NANITH777 <nanifulchany@gmail.com>
2023-03-17 14:13:36 +0300

    added functions

--- IEEF 754 floating point/main.c
+++ IEEF 754 floating point/main.c
@@ -22,6 +22,41 @@ MyStruct division1(float number1)
     return NumDecomposition;
 }
 
+
+MyStruct division2(double number2)
+{
+    MyStruct NumDecomposition;
+    NumDecomposition.tamsayi =0;
+    NumDecomposition.sayi2=0.0;
+    while( NumDecomposition.tamsayi <= number2)
+    {
+        NumDecomposition.tamsayi++;
+    }
+    NumDecomposition.tamsayi -=1;
+    NumDecomposition.sayi2= number2 - NumDecomposition.tamsayi;
+    return NumDecomposition;
+}
+
+int highOfarray1(int number1)
+{
+    int count=0;
+    while( number1 >=1) {
+        number1/=2;
+        count++;
+    }
+    return count;
+}
+
+int highOfarray2(int number2)
+{
+    int count=0;
+    while( number2 >=1) {
+        number2/=2;
+        count++;
+    }
+    return count;
+}
+
 int main()
 {
     printf("Hello world!\n");
